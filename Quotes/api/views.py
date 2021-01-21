a72bfb7a06f08f7f7d3bc69586629dca860339d3
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-01-21 22:44:45 +0200

    Add view code for GET quote request

--- Quotes/api/views.py
+++ Quotes/api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .models import Quote
+from .serializers import QuoteSerializer
+
+@api_view(['GET'])
+def quote(request):
+    if request.method == 'GET':
+        # TODO: get random quote
+        quote = Quote.objects.filter(id=1)
+        serializer = QuoteSerializer(quote, many=True)
+        return Response(serializer.data)
