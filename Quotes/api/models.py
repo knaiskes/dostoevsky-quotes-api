307199c0dee1c4d981e7634b1c7982dcab3d7161
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-01-22 18:52:24 +0200

    Add Novel model

--- Quotes/api/models.py
+++ Quotes/api/models.py
@@ -1,5 +1,44 @@
 from django.db import models
 
+class Novel(models.Model):
+    POOR_FOLK = 'Poor Folk'
+    THE_DOUBLE = 'The Double'
+    THE_LANDLADY = 'The Landlady'
+    NETOCHKA_NEZVANOVA = 'Netochka Nezvanova'
+    UNCLES_DREAM = "Unclue's Dream"
+    THE_VILLAGE_OF_STEPANCHIKOVO = 'The Village of Stepanchikovo'
+    HUMILIATED_AND_INSULTED = 'Humiliated and Insulted'
+    THE_HOUSE_OF_THE_DEAD = 'The House of the Dead'
+    NOTES_FROM_UNDERGROUND = 'Notes from Underground'
+    CRIME_AND_PUNISHMENT = 'Crime and Punishment'
+    THE_GAMBLER = 'The Gambler'
+    THE_IDIOT = 'The Idiot'
+    THE_ETERNAL_HUSBAND = 'The Eternal Husband'
+    THE_POSSESSED = 'The Possessed'
+    THE_ADOLESCENT = 'The Adolescent'
+    THE_BROTHERS_KARAMAZOV = 'The Brothers Karamazon'
+
+    NOVEL_LIST_CHOICES = [
+    (POOR_FOLK, 'Poor Folk'),
+    (THE_DOUBLE, 'The Double'),
+    (THE_LANDLADY, 'The Landlady'),
+    (NETOCHKA_NEZVANOVA, 'Netochka Nezvanova'),
+    (UNCLES_DREAM, "Unclue's Dream"),
+    (THE_VILLAGE_OF_STEPANCHIKOVO, 'The Village of Stepanchikovo'),
+    (HUMILIATED_AND_INSULTED, 'Humiliated and Insulted'),
+    (THE_HOUSE_OF_THE_DEAD, 'The House of the Dead'),
+    (NOTES_FROM_UNDERGROUND, 'Notes from Underground'),
+    (CRIME_AND_PUNISHMENT, 'Crime and Punishment'),
+    (THE_GAMBLER, 'The Gambler'),
+    (THE_IDIOT, 'The Idiot'),
+    (THE_ETERNAL_HUSBAND, 'The Eternal Husband'),
+    (THE_POSSESSED, 'The Possessed'),
+    (THE_ADOLESCENT, 'The Adolescent'),
+    (THE_BROTHERS_KARAMAZOV, 'The Brothers Karamazon'),
+    ]
+
+    novel = models.CharField(max_length=28, choices=NOVEL_LIST_CHOICES)
+
 class Quote(models.Model):
     text = models.CharField(max_length=120)
     # TODO: add -> book_title
