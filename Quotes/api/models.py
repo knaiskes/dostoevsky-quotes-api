0d4262c0cd0c2101ef0385b416168a0ec5a9c213
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-01-22 19:23:34 +0200

    Remove Novel model and use choices from within Quotes model

--- Quotes/api/models.py
+++ Quotes/api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Novel(models.Model):
+class Quote(models.Model):
     POOR_FOLK = 'Poor Folk'
     THE_DOUBLE = 'The Double'
     THE_LANDLADY = 'The Landlady'
@@ -19,29 +19,26 @@ class Novel(models.Model):
     THE_BROTHERS_KARAMAZOV = 'The Brothers Karamazon'
 
     NOVEL_LIST_CHOICES = [
-    (POOR_FOLK, 'Poor Folk'),
-    (THE_DOUBLE, 'The Double'),
-    (THE_LANDLADY, 'The Landlady'),
-    (NETOCHKA_NEZVANOVA, 'Netochka Nezvanova'),
-    (UNCLES_DREAM, "Unclue's Dream"),
-    (THE_VILLAGE_OF_STEPANCHIKOVO, 'The Village of Stepanchikovo'),
-    (HUMILIATED_AND_INSULTED, 'Humiliated and Insulted'),
-    (THE_HOUSE_OF_THE_DEAD, 'The House of the Dead'),
-    (NOTES_FROM_UNDERGROUND, 'Notes from Underground'),
-    (CRIME_AND_PUNISHMENT, 'Crime and Punishment'),
-    (THE_GAMBLER, 'The Gambler'),
-    (THE_IDIOT, 'The Idiot'),
-    (THE_ETERNAL_HUSBAND, 'The Eternal Husband'),
-    (THE_POSSESSED, 'The Possessed'),
-    (THE_ADOLESCENT, 'The Adolescent'),
-    (THE_BROTHERS_KARAMAZOV, 'The Brothers Karamazon'),
+        (POOR_FOLK, 'Poor Folk'),
+        (THE_DOUBLE, 'The Double'),
+        (THE_LANDLADY, 'The Landlady'),
+        (NETOCHKA_NEZVANOVA, 'Netochka Nezvanova'),
+        (UNCLES_DREAM, "Unclue's Dream"),
+        (THE_VILLAGE_OF_STEPANCHIKOVO, 'The Village of Stepanchikovo'),
+        (HUMILIATED_AND_INSULTED, 'Humiliated and Insulted'),
+        (THE_HOUSE_OF_THE_DEAD, 'The House of the Dead'),
+        (NOTES_FROM_UNDERGROUND, 'Notes from Underground'),
+        (CRIME_AND_PUNISHMENT, 'Crime and Punishment'),
+        (THE_GAMBLER, 'The Gambler'),
+        (THE_IDIOT, 'The Idiot'),
+        (THE_ETERNAL_HUSBAND, 'The Eternal Husband'),
+        (THE_POSSESSED, 'The Possessed'),
+        (THE_ADOLESCENT, 'The Adolescent'),
+        (THE_BROTHERS_KARAMAZOV, 'The Brothers Karamazon'),
     ]
 
-    novel = models.CharField(max_length=28, choices=NOVEL_LIST_CHOICES)
-
-class Quote(models.Model):
     text = models.CharField(max_length=120)
-    # TODO: add -> book_title
+    novel = models.CharField(max_length=28, choices=NOVEL_LIST_CHOICES)
 
     def __str__(self):
         return self.text
