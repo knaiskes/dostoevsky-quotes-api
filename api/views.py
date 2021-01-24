a3d7013082b9fe966b6318e00d4a04c778526a08
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-01-24 13:49:34 +0200

    Get random quote

--- api/views.py
+++ api/views.py
@@ -8,7 +8,9 @@ from .serializers import QuoteSerializer
 @api_view(['GET'])
 def quote(request):
     if request.method == 'GET':
-        # TODO: get random quote
-        quote = Quote.objects.filter(id=1)
+        from random import choice
+        pks = Quote.objects.values_list('pk', flat=True)
+        random_pk = choice(pks)
+        quote = Quote.objects.filter(pk=random_pk)
         serializer = QuoteSerializer(quote, many=True)
         return Response(serializer.data)
