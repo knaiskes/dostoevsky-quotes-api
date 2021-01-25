be1a643d2f6390572f14339426eb088134c9f12e
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-01-25 13:48:54 +0200

    Sort alphabetically novels

--- api/models.py
+++ api/models.py
@@ -1,40 +1,40 @@
 from django.db import models
 
 class Quote(models.Model):
-    POOR_FOLK = 'Poor Folk'
-    THE_DOUBLE = 'The Double'
-    THE_LANDLADY = 'The Landlady'
-    NETOCHKA_NEZVANOVA = 'Netochka Nezvanova'
-    UNCLES_DREAM = "Unclue's Dream"
-    THE_VILLAGE_OF_STEPANCHIKOVO = 'The Village of Stepanchikovo'
-    HUMILIATED_AND_INSULTED = 'Humiliated and Insulted'
-    THE_HOUSE_OF_THE_DEAD = 'The House of the Dead'
-    NOTES_FROM_UNDERGROUND = 'Notes from Underground'
     CRIME_AND_PUNISHMENT = 'Crime and Punishment'
-    THE_GAMBLER = 'The Gambler'
-    THE_IDIOT = 'The Idiot'
-    THE_ETERNAL_HUSBAND = 'The Eternal Husband'
-    THE_POSSESSED = 'The Possessed'
+    HUMILIATED_AND_INSULTED = 'Humiliated and Insulted'
+    NETOCHKA_NEZVANOVA = 'Netochka Nezvanova'
+    NOTES_FROM_UNDERGROUND = 'Notes from Underground'
+    POOR_FOLK = 'Poor Folk'
     THE_ADOLESCENT = 'The Adolescent'
     THE_BROTHERS_KARAMAZOV = 'The Brothers Karamazon'
+    THE_DOUBLE = 'The Double'
+    THE_ETERNAL_HUSBAND = 'The Eternal Husband'
+    THE_GAMBLER = 'The Gambler'
+    THE_HOUSE_OF_THE_DEAD = 'The House of the Dead'
+    THE_IDIOT = 'The Idiot'
+    THE_LANDLADY = 'The Landlady'
+    THE_POSSESSED = 'The Possessed'
+    THE_VILLAGE_OF_STEPANCHIKOVO = 'The Village of Stepanchikovo'
+    UNCLES_DREAM = "Unclue's Dream"
 
     NOVEL_LIST_CHOICES = [
-        (POOR_FOLK, 'Poor Folk'),
-        (THE_DOUBLE, 'The Double'),
-        (THE_LANDLADY, 'The Landlady'),
-        (NETOCHKA_NEZVANOVA, 'Netochka Nezvanova'),
-        (UNCLES_DREAM, "Unclue's Dream"),
-        (THE_VILLAGE_OF_STEPANCHIKOVO, 'The Village of Stepanchikovo'),
-        (HUMILIATED_AND_INSULTED, 'Humiliated and Insulted'),
-        (THE_HOUSE_OF_THE_DEAD, 'The House of the Dead'),
-        (NOTES_FROM_UNDERGROUND, 'Notes from Underground'),
-        (CRIME_AND_PUNISHMENT, 'Crime and Punishment'),
-        (THE_GAMBLER, 'The Gambler'),
-        (THE_IDIOT, 'The Idiot'),
-        (THE_ETERNAL_HUSBAND, 'The Eternal Husband'),
-        (THE_POSSESSED, 'The Possessed'),
-        (THE_ADOLESCENT, 'The Adolescent'),
-        (THE_BROTHERS_KARAMAZOV, 'The Brothers Karamazon'),
+    (CRIME_AND_PUNISHMENT, 'Crime and Punishment'),
+    (HUMILIATED_AND_INSULTED, 'Humiliated and Insulted'),
+    (NETOCHKA_NEZVANOVA, 'Netochka Nezvanova'),
+    (NOTES_FROM_UNDERGROUND, 'Notes from Underground'),
+    (THE_ADOLESCENT, 'The Adolescent'),
+    (THE_BROTHERS_KARAMAZOV, 'The Brothers Karamazon'),
+    (THE_DOUBLE, 'The Double'),
+    (THE_ETERNAL_HUSBAND, 'The Eternal Husband'),
+    (THE_GAMBLER, 'The Gambler'),
+    (THE_HOUSE_OF_THE_DEAD, 'The House of the Dead'),
+    (THE_IDIOT, 'The Idiot'),
+    (THE_LANDLADY, 'The Landlady'),
+    (THE_POSSESSED, 'The Possessed'),
+    (THE_VILLAGE_OF_STEPANCHIKOVO, 'The Village of Stepanchikovo'),
+    (UNCLES_DREAM, "Unclue's Dream"),
+    (POOR_FOLK, 'Poor Folk'),
     ]
 
     text = models.CharField(max_length=120, unique=True)
