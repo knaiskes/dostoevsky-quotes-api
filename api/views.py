1a6db7ce66bf1ed6a20970584596eb927c92f1f4
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-01-25 20:16:25 +0200

    Add get quote by id view

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 #from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 from .models import Quote
 from .serializers import QuoteSerializer
@@ -11,6 +12,17 @@ def quote(request):
         from random import choice
         pks = Quote.objects.values_list('pk', flat=True)
         random_pk = choice(pks)
-        quote = Quote.objects.filter(pk=random_pk)
-        serializer = QuoteSerializer(quote, many=True)
+        quote = Quote.objects.get(pk=random_pk)
+        serializer = QuoteSerializer(quote)
         return Response(serializer.data)
+
+@api_view(['GET'])
+def quote_by_id(request, pk):
+     try:
+         quote = Quote.objects.get(pk=pk)
+     except Quote.DoesNotExist:
+         return Response(status=status.HTTP_404_NOT_FOUND)
+
+     if request.method == 'GET':
+         serializer = QuoteSerializer(quote)
+         return Response(serializer.data)
