ac25fc39ed07e00e08d3f2205cd628ae4cea1e60
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-01-27 17:43:35 +0200

    Get quotes by novel

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,6 @@ app_name = 'api'
 urlpatterns = [
     path('quote/', views.quote, name='quote'),
     path('quotes', views.ApiQuoteListView.as_view(), name='quotes'),
+    path('quotes/novel/<novel>', views.ApiQuoteListByNovelView.as_view(), name='quotes_by_novel'),
     path('quotes/<pk>', views.quote_by_id, name='quote_by_id'),
 ]
