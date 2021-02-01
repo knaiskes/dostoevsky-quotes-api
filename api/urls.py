438c189e16eddccb66fb801ae458f273640526fa
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-02-01 20:51:07 +0200

    Fix quotes path

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ app_name = 'api'
 
 urlpatterns = [
     path('quotes/random', views.random, name='random'),
-    path('quotes', views.ApiQuoteListView.as_view(), name='quotes'),
+    path('quotes/', views.ApiQuoteListView.as_view(), name='quotes'),
     path('quotes/novel/<novel>', views.ApiQuoteListByNovelView.as_view(), name='quotes_by_novel'),
     path('quotes/<pk>', views.quote_by_id, name='quote_by_id'),
 ]
